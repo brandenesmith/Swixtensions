d2afedab6f08694a0fa8236be6a24f412d9a92e4
Branden Smith <smith.brandene@gmail.com>
2020-12-01 14:16:49 -0600

    Add animation completion view modifier.

--- Sources/Swixtensions/Extensions/View+Modifiers.swift
+++ Sources/Swixtensions/Extensions/View+Modifiers.swift
@@ -13,4 +13,14 @@ public extension View {
     func isHidden(_ hidden: Binding<Bool>) -> some View {
         return self.modifier(HiddenViewModifier(isHidden: hidden))
     }
+
+    /// Calls the completion handler whenever an animation on the given value completes.
+    /// taken from https://www.avanderlee.com/swiftui/withanimation-completion-callback/
+    /// - Parameters:
+    ///   - value: The value to observe for animations.
+    ///   - completion: The completion callback to call once the animation completes.
+    /// - Returns: A modified `View` instance with the observer attached.
+    func onAnimationCompleted<Value: VectorArithmetic>(for value: Value, completion: @escaping () -> Void) -> ModifiedContent<Self, AnimationCompletionObserverModifier<Value>> {
+        return modifier(AnimationCompletionObserverModifier(observedValue: value, completion: completion))
+    }
 }
